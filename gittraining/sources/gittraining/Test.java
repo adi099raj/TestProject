9be61e5e8d4b2a4b9ab0715cb684aeacd95300d6
adi099raj <adityarajsaharsa@gmail.com>
2018-09-13 18:22:16 +0530

    const

--- gittraining/sources/gittraining/Test.java
+++ gittraining/sources/gittraining/Test.java
@@ -3,12 +3,15 @@ package gittraining;
 public class Test {
 
 	public Test() {
+		System.out.println("A()");
 		// TODO Auto-generated constructor stub
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Test a1 = new Test();
+		System.out.println("---------");
 	}
 
 }
